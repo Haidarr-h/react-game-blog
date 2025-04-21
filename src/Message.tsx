3abe490bc7915d405d95db41c8655ad36b6ab2e7
haidarniv <haidarapps12@gmail.com>
2025-04-21 09:40:36 +0700

    first commit

---
+++ src/Message.tsx
@@ -0,0 +1,12 @@
+
+// PascalCasing
+function Message() {
+
+    const name = 'Haidare';
+    if (name)
+        return <h1>Hello my good sir {name}</h1>
+
+    return <h1>Hello World! Shut the fuck Up !!!!!!</h1>;
+}
+
+export default Message;
